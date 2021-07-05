167b2f110600a9b5a569348b5ccf5617159cbaec
Diego Alvarez <andresdaap2015@gmail.com>
2021-07-05 14:06:30 -0400

    Add background on scroll to navbar

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+
+document.onscroll= () => {
+	const body = document.querySelector("body");
+	const navbar = document.querySelector(".navbar");
+	let positionScroll = body.getBoundingClientRect().y;
+
+	if (positionScroll < -10) {
+		navbar.classList.add("navbar-active");
+	} else {
+		navbar.classList.remove("navbar-active");
+	}
+}
